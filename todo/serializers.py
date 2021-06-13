cc54f0909021bc088555dd304bdf85dbe8936066
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-06-13 20:04:10 +0100

    create serializers for api

---
+++ todo/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from rest_framework import serializers
+
+from .models import ToDo
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
+
