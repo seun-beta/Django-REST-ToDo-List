7a33e0ec6bad42e05fbd95b0d38443ed99f3601e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-06-13 20:03:56 +0100

    add models for todo list

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=500)
+    url = models.CharField(max_length=500, null=True, default=None)
+    completed = models.BooleanField(null=True, default=None)
+    order = models.IntegerField(null=True, default=None)
+    
