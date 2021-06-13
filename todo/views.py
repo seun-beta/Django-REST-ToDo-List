0d2afdc9fe285dfd35738c30128c9caf5faa0b39
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-06-13 20:04:37 +0100

    create views for todo app

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import ToDo
+from .serializers import ToDoSerializer
 
-# Create your views here.
+
+class ToDoList(APIView):
+    def post(self, request):
+        todo = ToDoSerializer(request.data)
+        return Response(todo.data)
+
+class ToDoOne(APIView):
+    pass
