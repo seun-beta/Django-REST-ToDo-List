29a5c15d26e2209dfb3a29650cc06b899b42cd77
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-06-17 22:02:39 +0100

    add method for get and delete

--- todo/views.py
+++ todo/views.py
@@ -6,9 +6,19 @@ from .serializers import ToDoSerializer
 
 
 class ToDoList(APIView):
+
+    def get(self, request):
+        return Response(None)
+
     def post(self, request):
-        todo = ToDoSerializer(request.data)
+        todo = ToDoSerializer(data=request.data)
+        if todo.is_valid():
+            todo.save()
         return Response(todo.data)
+
+    def delete(self, request):
+        return Response(None)
+
 
 class ToDoOne(APIView):
     pass
